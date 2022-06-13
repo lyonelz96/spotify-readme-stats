1d70ed3affb3d318a8893cc11fd963eb2c08f126
Lyonel Zamora <lyonelz516@gmail.com>
2022-06-13 17:11:31 -0400

    generate static svg templaes

--- index.mjs
+++ index.mjs
@@ -146,3 +146,107 @@ app.get('/top-items/:type', async (req, res) => {
 //  __..  ..__    __. ,    ._._
 // (__ \  /[ __  (__ -+-. .|,|,
 // .__) \/ [_./  .__) | (_|| | 
+
+const genStyle = () => {
+    return `
+        <style>
+            svg {
+                width: 400px;
+                height: 400px;
+            }
+
+            rect,
+            foreignObject,
+            container {
+                width: 100%;
+                height:100%;
+            }
+
+            rect {
+                rx: 10px;
+                ry: 10px;
+                fill: #1F1F28;
+            }
+
+            .container {
+                display: flex;
+                flex-direction: column;
+            }
+
+            .container-header-item {
+                display: flex;
+                flex-direction: row;
+                justify-content: center;
+                align-items: center;
+            }
+
+            .container-header-item>img,
+            .container-media-item>img {
+                margin: 1rem;
+            }
+
+            .container-media-item {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+
+                margin-left: 2rem;
+            }
+
+            .container-media-item>h3 {
+                flex-grow: 1;
+                align-self: center;
+            }
+
+            hr {
+                width: 100%;
+                border: 0;
+                height: 2px;
+                background-image: linear-gradient(to right, transparent, #938AA9, transparent);
+            }
+
+            h1,
+            h2,
+            h3,
+            h4,
+            h5,
+            h6 {
+                color: #DCD7BA;
+                font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            }
+        </style>
+    `
+}
+const genMediaObject = () => {
+    return `
+    <div class="container">
+        <div class="container-header-item">
+            <img src="https://place-hold.it/50x50" alt="Spotify Logo">
+            <h1>Title</h1>
+        </div>
+
+        <hr>
+
+        <div class="container-media-item">
+            <img src="https://place-hold.it/50x50" alt="Spotify Logo">
+            <h4>album/track/artist name</h4>
+        </div>
+    </div>
+    `
+}
+
+const genSVG = () => {
+    return `
+        <svg>
+            ${genStyle()}
+            <rect />
+            <foreignObject>
+                ${genMediaObject()}
+            </foreignObject>
+        </svg>
+    `
+}
+
+app.get('/svg', (req, res) => {
+    res.send(genSVG())
+})
