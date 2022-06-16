907c4050f8dd9d1b39fb8e2c8a3d2743d2900892
Lyonel Zamora <lyonelz516@gmail.com>
2022-06-15 23:28:52 -0400

    fix auth redirect_uri

--- src/auth/auth.constants.mjs
+++ src/auth/auth.constants.mjs
@@ -1,4 +1,4 @@
-export const REDIRECT_URI = process.env.AUTH_REDIRECT_URI || 'localhost:3000/auth/callback'
+export const REDIRECT_URI = process.env.AUTH_REDIRECT_URI || 'http://localhost:3000/auth/callback'
 export const CLIENT_ID = process.env.CLIENT_ID
 export const CLIENT_SECRET = process.env.CLIENT_SECRET
 export const AUTHORIZE_ENDPOINT = 'https://accounts.spotify.com/authorize'
