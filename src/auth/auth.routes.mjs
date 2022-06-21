859c7099593783ffaba89a6f25540ce4b070b13d
Lyonel Zamora <lyonelz516@gmail.com>
2022-06-21 02:20:37 -0400

    add auth to endpoint path

--- src/auth/auth.routes.mjs
+++ src/auth/auth.routes.mjs
@@ -3,7 +3,7 @@ import authController from './auth.controller.mjs'
 
 const authRouter = express.Router()
 
-authRouter.get('/login', authController.login)
+authRouter.get('/auth/login', authController.login)
 authRouter.get('/auth/callback', authController.authCallback)
 
 export default authRouter
