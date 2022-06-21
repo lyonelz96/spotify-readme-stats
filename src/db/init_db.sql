d6062fc6a6e712e234a78cd9b44ed35ab20c4bf1
Lyonel Zamora <lyonelz516@gmail.com>
2022-06-20 23:03:51 -0400

    add docker compose info for init of db

---
+++ src/db/init_db.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS session;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    spotify_id TEXT PRIMARY KEY,
+    refresh_token TEXT NOT NULL,
+    svg_recently_played TEXT,
+    svg_top_tracks TEXT,
+    svg_top_artists TEXT
+);
