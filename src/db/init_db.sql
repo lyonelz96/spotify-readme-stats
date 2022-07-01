5d081742af68fa7310180249d47f327c89b9fe68
Lyonel Zamora <lyonelz516@gmail.com>
2022-07-01 08:54:00 -0400

    fix some typos

--- src/db/init_db.sql
+++ src/db/init_db.sql
@@ -5,21 +5,22 @@ DROP TABLE IF EXISTS svg_types;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    spotify_id UNIQUE TEXT NOT NULL,
-    refresh_token TEXT NOT NULL,
+    spotify_id TEXT UNIQUE NOT NULL,
+    refresh_token TEXT NOT NULL
+);
+
+CREATE TABLE svg_types (
+    id SERIAL PRIMARY KEY,
+    svg_type TEXT NOT NULL
 );
 
 CREATE TABLE svgs (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users ON DELETE CASCADE,
-    svg_type_id INTEGER REFERENCES svg_types,
+    svg_type_id INTEGER REFERENCES svg_types
 );
 
-CREATE TABLE svg_types (
-    id SERIAL PRIMARY KEY,
-    svg_type TEXT,
-);
 
-INSERT INTO svg_types VALUES ("TopTracks")
-INSERT INTO svg_types VALUES ("TopArtists")
-INSERT INTO svg_types VALUES ("RecentlyPlayed")
+INSERT INTO svg_types VALUES (DEFAULT, 'TopTracks');
+INSERT INTO svg_types VALUES (DEFAULT, 'TopArtists');
+INSERT INTO svg_types VALUES (DEFAULT, 'RecentlyPlayed');
