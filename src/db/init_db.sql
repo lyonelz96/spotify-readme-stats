5e83e8d68bb0b9b6318adeb276aa8436289079d0
Lyonel Zamora <lyonelz516@gmail.com>
2022-07-01 08:11:37 -0400

    update init_db script

--- src/db/init_db.sql
+++ src/db/init_db.sql
@@ -1,10 +1,25 @@
 DROP TABLE IF EXISTS session;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS svgs;
+DROP TABLE IF EXISTS svg_types;
 
 CREATE TABLE users (
-    id TEXT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    spotify_id UNIQUE TEXT NOT NULL,
     refresh_token TEXT NOT NULL,
-    svg_recently_played TEXT,
-    svg_top_tracks TEXT,
-    svg_top_artists TEXT
 );
+
+CREATE TABLE svgs (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
+    svg_type_id INTEGER REFERENCES svg_types,
+);
+
+CREATE TABLE svg_types (
+    id SERIAL PRIMARY KEY,
+    type TEXT,
+);
+
+INSERT INTO svg_types VALUES ("TopTracks")
+INSERT INTO svg_types VALUES ("TopArtists")
+INSERT INTO svg_types VALUES ("RecentlyPlayed")
