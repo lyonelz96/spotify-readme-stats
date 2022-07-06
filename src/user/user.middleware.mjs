cdc750595f5823e7096848b329d8857b1bcece8d
Lyonel Zamora <lyonelz516@gmail.com>
2022-07-06 11:39:19 -0400

    add user middleware

---
+++ src/user/user.middleware.mjs
@@ -0,0 +1,29 @@
+import userModel from './user.model.mjs'
+
+const userMiddleware = {}
+
+userMiddleware.checkIfUserExistsInDB = async (req, res, next) => {
+    const spotify_id = req.params.spotify_id
+
+    const user = await userModel.find(spotify_id)
+
+    if (user) {
+        next()
+    } else {
+        res.send('User not found!')
+    }
+}
+
+userMiddleware.checkIfUserIsTheSameAsSession = (req, res, next) => {
+    if (req.session.user) {
+        const spotify_id = req.params.spotify_id
+        const session_spotify_id = req.session.user.spotify_id
+
+        if (spotify_id === session_spotify_id) {
+            req.userSameAsSession = true
+        }
+    }
+    next()
+}
+
+export default userMiddleware
