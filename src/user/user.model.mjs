96548a9131322f81d2a88857798273a9a073166e
Lyonel Zamora <lyonelz516@gmail.com>
2022-07-06 11:36:57 -0400

    add more svg and refresh token functionality to user model

--- src/user/user.model.mjs
+++ src/user/user.model.mjs
@@ -14,35 +14,21 @@ userModel.find = async (spotify_id) => {
 
 userModel.create = async (spotify_id, refresh_token) => {
     try {
-        await db.query('INSERT INTO users VALUES ($1, $2)', [spotify_id, refresh_token])
+        await db.query('INSERT INTO users (spotify_id, refresh_token) VALUES ($1, $2)', [spotify_id, refresh_token])
     } catch (error) {
         console.error(error)
     }
 }
 
-userModel.update = async (spotify_id, update) => {
-    let updateStr = 'UPDATE users SET '
-    let keysOrdered = []
-
-    Object.keys(update).forEach(key => {
-        keysOrdered.push(key)
-        updateStr += `${key} = $${keysOrdered.length},`
-    })
-
-    updateStr = updateStr.slice(0, -1)
-
-    updateStr += ` WHERE spotify_id = '${spotify_id}'`
-
-    let values = keysOrdered.map(k => update[k])
-
+userModel.updateRefreshToken = async (spotify_id, refresh_token) => {
     try {
-        await db.query(updateStr, values)
+        await db.query('UPDATE users SET refresh_token = $1 WHERE spotify_id = $2', [refresh_token, spotify_id])
     } catch (error) {
         console.error(error)
     }
 }
 
-userModel.svgs = async (spotify_id) => {
+userModel.getAllSVGS = async (spotify_id) => {
     try {
         const user_id = (await userModel.find(spotify_id)).id
         const svgs = (await db.query('SELECT svg, request_date, svg_type FROM svgs JOIN svg_types ON svgs.svg_type_id = svg_types.id AND user_id = $1', [user_id])).rows
@@ -53,10 +39,44 @@ userModel.svgs = async (spotify_id) => {
     }
 }
 
-userModel.svg = async (spotify_id, type) => {
-    const svgs = await userModel.svgs(spotify_id)
+userModel.getSVG = async (spotify_id, type) => {
+    try {
+        const svgs = await userModel.getAllSVGS(spotify_id)
 
-    return svgs.find(svg => svg.svg_type === type)
+        if (svgs.length === 0) {
+            return null
+        }
+        else {
+            return svgs.find(svg => svg.svg_type === type)
+        }
+
+    } catch (error) {
+        console.error(error)
+    }
 }
 
+userModel.updateSVG = async (spotify_id, type, svg) => {
+    try {
+        const type_id = (await db.query('SELECT id FROM svg_types WHERE svg_type = $1', [type])).rows[0].id
+        const user_id = (await userModel.find(spotify_id)).id
+
+        await db.query('UPDATE svgs SET svg = $1 WHERE user_id = $2 AND svg_type_id = $3', [svg, user_id, type_id])
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+userModel.createSVG = async (spotify_id, type, svg) => {
+    try {
+        const type_id = (await db.query('SELECT id FROM svg_types WHERE svg_type = $1', [type])).rows[0].id
+        const user_id = (await userModel.find(spotify_id)).id
+        const request_date = new Date().getTime()
+
+        console.log(type_id, user_id, request_date)
+
+        await db.query('INSERT INTO svgs (user_id, svg_type_id, svg, request_date) VALUES ($1, $2, $3, $4)', [user_id, type_id, svg, request_date])
+    } catch (error) {
+        console.error(error)
+    }
+}
 export default userModel
