18c904a93068592bea19dfd3b87c427d760a49fd
Lyonel Zamora <lyonelz516@gmail.com>
2022-07-08 11:31:40 -0400

    remove ssl on development

--- src/db/index.mjs
+++ src/db/index.mjs
@@ -3,12 +3,17 @@ import { DABATASE_URL } from './db.constants.mjs'
 
 const db = {}
 
-db.pool = new pg.Pool({
-    connectionString: DABATASE_URL,
-    ssl: {
+const config = {
+    connectionString: DABATASE_URL
+}
+
+if (process.env.NODE_ENV === 'production') {
+    config.ssl = {
         rejectUnauthorized: false
     }
-})
+}
+
+db.pool = new pg.Pool(config)
 
 db.query = (sql, params) => db.pool.query(sql, params)
 
