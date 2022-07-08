a28e332ac3681d6a77035fb04928481696476d42
Lyonel Zamora <lyonelz516@gmail.com>
2022-07-08 11:55:56 -0400

    add utils to convert images to base 64

--- src/utils/index.mjs
+++ src/utils/index.mjs
@@ -5,3 +5,23 @@ const __dirname = new URL('../', import.meta.url).pathname
 export const absPathToFile = relPathToFile => {
     return resolve(__dirname + relPathToFile)
 }
+
+import axios from 'axios'
+import { extname } from 'path'
+import { readFileSync } from 'fs'
+
+export const webBase64Img = async (url) => {
+    const image = await axios.get(url, { responseType: 'arraybuffer' });
+    const raw = Buffer.from(image.data).toString('base64');
+    const base64Image = 'data:' + image.headers['content-type'] + ';base64,' + raw;
+
+    return base64Image
+}
+
+export const localBase64Img = (img) => {
+    const path = absPathToFile(img)
+    const type = extname(path).slice(1)
+    const base64Image = 'data:image/' + type + ';base64,' + readFileSync(path, 'base64')
+
+    return base64Image
+}
